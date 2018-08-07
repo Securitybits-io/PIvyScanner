8407820180e935655a6778e255f26143fef9d9db
Christoffer Claesson <switch88@gmail.com>
2018-08-07 15:44:01 +0200

    Liscence Added

--- PIvyScanner.py
+++ PIvyScanner.py
@@ -1,3 +1,40 @@
-import sys
+'''
+MIT Liscence
+Copyright (c) 2018 Christoffer Claesson @ Securitybits.io
 
-print('Hello World' + sys.version)
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+#TODO: Parse target Single IP
+#TODO: Parse Target CIDR Range
+#TODO: Parse targets from file
+#TODO: Parse Port range
+#TODO: Printing of data
+#TODO: write to file?
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+
+    pass
+
+if __name__ == '__main__':
+    main()
