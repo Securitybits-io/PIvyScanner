9badf675c17a02e29a5d786a31c2c11eb8dae775
Christoffer Claesson <switch88@gmail.com>
2018-08-07 21:28:47 +0200

    v0.2

--- PIvyScanner.py
+++ PIvyScanner.py
@@ -21,24 +21,69 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+#Statics
+prog_desc = '''
+Python script to scan after C&C Servers running Poison Ivy,
+which is vulnerable to a Remote Code Execution bug. Exploit-DB: 39907 & 19613
+'''
+
 #TODO: Parse target Single IP
 #TODO: Parse Target CIDR Range
 #TODO: Parse targets from file
 #TODO: Parse Port range
-#TODO: Printing of data
+#TODO: Printing of data stdout
 #TODO: write to file?
 
 import argparse
-import netaddr
+from netaddr import IPNetwork
 
+def createIpRange(cidr): #NOTE: 192.168.0.0/16 = 0.5MB ram
+    range = []
+    for ip in IPNetwork(cidr):
+        range.append(ip)
+    return range
+
+def createPortRange(range):
+    ports = []
+    if(range == None):
+        ports = [3460]
+    else:
+        ports = range.split(",")
+    return ports
+
+def ivyScan(ip,port):
+    pass
+
+def initArgParser():
+    parser = argparse.ArgumentParser(prog="PIvyScanner.py", usage='./%(prog)s [options]', description=prog_desc )
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-i', '--ip',    type=str, help='Input target ip')#Single Target
+    group.add_argument('-r', '--range', type=str, help='Input target ip in CIDR Notation eg. 127.0.0.1/24')#CIDR Range
+    group.add_argument('-f', '--file',  type=str, help='Choose file with one IP on each line')#Targets from file
+    parser.add_argument('-p', '--ports', type=str, help='Input port range separated by comma. (Default=3460)')#Ports
+    return parser.parse_args()
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.parse_args()
+    args = initArgParser()
+    ports = createPortRange(args.ports)
+    IPs = []
 
-    pass
+    if ((args.ip or args.range or args.file) == None):
+        print("Please specify an IP to scan")
+        return
+    elif(args.ip != None):
+        pass
+    elif(args.range != None):
+        pass
+    elif(args.file != None):
+        pass
+
+    
+    for ip in IPs:
+        for port in ports:
+            print("Scanning: %s:%s" % ip, str(port) )
+
+    return
 
 if __name__ == '__main__':
     main()
-
-print("it works!")
