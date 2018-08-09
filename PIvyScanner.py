7df97a7b1c4934f9b318f35d9f2864fa3a0e17f0
Christoffer Claesson <switch88@gmail.com>
2018-08-09 23:23:26 +0200

    v0.5 Basic scanning done

--- PIvyScanner.py
+++ PIvyScanner.py
@@ -21,20 +21,27 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
+__author__ = 'Christoffer Claesson (Christoffer.Claesson@Securitybits.io)'
+__copyright__ = 'Copyright (c) 2018 Christoffer Claesson'
+__license__ = 'MIT Liscence'
+__vcs_id__ = '$Id$'
+__version__ = '1.0.0' #Versioning: http://www.python.org/dev/peps/pep-0386/
+
 #Statics
 prog_desc = '''
 Python script to scan after C&C Servers running Poison Ivy,
 which is vulnerable to a Remote Code Execution bug. Exploit-DB: 39907 & 19613
 '''
 
-#TODO: Parse Port range
-#TODO: Printing of data stdout
+#TODO: Threading?
 #TODO: write to file?
 
 import argparse
 import iptools
+import socket
 
 def createIpRange(cidr): #NOTE: 192.168.0.0/16 = 0.5MB ram
+    print("Generating IP Range...")
     range = []
     for ip in iptools.IpRange(cidr):
         range.append(ip)
@@ -49,41 +56,68 @@ def createPortRange(range):
     return ports
 
 def ivyScan(ip,port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(3)
+    
+    try:
+        sock.connect((ip,int(port)))
+        sData = "\x00" * 0x100
+        sock.sendall(sData)
+        rData = sock.recv(0x100)
+        if len(rData) != 0x100:
+            sock.close()
+            return
+        else:
+            rData = sock.recv(0x4)
+            sock.close()
+        
+        if rData != "\xD0\x15\x00\x00":
+            return
+        else:
+            print("%s:%d - Vulnerable" % (ip,port))
+        
+    except socket.timeout as e:
+        print("Scanning IP: %s:%d" % (ip,port),end='\r')
+    except socket.error as e:
+        pass
     return
 
 def scan(ipList, portList):
+    for target in ipList:
+        for port in portList:
+            ivyScan(target,port)
     return
 
 def initArgParser():
     parser = argparse.ArgumentParser(prog="PIvyScanner.py", usage='./%(prog)s [options]', description=prog_desc )
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-i', '--ip',    type=str, help='Input target ip')#Single Target
-    group.add_argument('-r', '--range', type=str, help='Input target ip in CIDR Notation eg. 127.0.0.1/24')#CIDR Range
-    group.add_argument('-f', '--file',  type=str, help='Choose file with one IP on each line')#Targets from file
-    parser.add_argument('-p', '--ports', type=str, help='Input port range separated by comma. (Default=3460)')#Ports
+    group.add_argument('-i',  '--ip',       type=str, help='Input target ip')#Single Target
+    group.add_argument('-r',  '--range',    type=str, help='Input target ip in CIDR Notation eg. 127.0.0.1/24')#CIDR Range
+    #group.add_argument('-f',  '--file',     type=str, help='Choose file with one IP on each line')#Targets from file
+    parser.add_argument('-p', '--ports',    type=str, help='Input port range separated by comma. (Default=3460)')#Ports
     return parser.parse_args()
 
 def main():
-    args = initArgParser()
-    ports = createPortRange(args.ports)
-    IPs = []
+    try:
+        args = initArgParser()
+        ports = createPortRange(args.ports)
+        IPs = []
 
-    if ((args.ip or args.range or args.file) == None):
-        print("Please specify an IP to scan")
+        if ((args.ip or args.range or args.file) == None):
+            print("Please specify an IP to scan")
+            return
+        elif(args.ip != None):
+            IPs.append(args.ip)
+        elif(args.range != None):
+            IPs = createIpRange(args.range)
+        elif(args.file != None):
+            with open(args.file) as targetFile:
+                IPs = [line.rstrip() for line in targetFile]
+
+        scan(IPs, ports)
         return
-    elif(args.ip != None):
-        IPs.append(args.ip)
-    elif(args.range != None):
-        IPs = createIpRange(args.range)
-    elif(args.file != None):
-        with open(args.file) as targetFile:
-            IPs = [line.rstrip() for line in targetFile]
-
-    for target in IPs:
-        for port in ports:
-            print(target+":"+str(port))
-
-    return
-
+    except(KeyboardInterrupt,SystemExit):
+        pass
+    
 if __name__ == '__main__':
     main()
